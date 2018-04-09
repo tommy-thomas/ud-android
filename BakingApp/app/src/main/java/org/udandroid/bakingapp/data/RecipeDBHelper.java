059edabfa1705cd4499da836a002fbfd8ed2a872
tommy-thomas <tommyt@uchicago.edu>
2018-04-09 17:24:52 -0500

    Cleaning up widget.

--- BakingApp/app/src/main/java/org/udandroid/bakingapp/data/RecipeDBHelper.java
+++ BakingApp/app/src/main/java/org/udandroid/bakingapp/data/RecipeDBHelper.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static org.udandroid.bakingapp.data.RecipeContract.RecipeEntry.COLUMN_ID;
+import static org.udandroid.bakingapp.data.RecipeContract.RecipeEntry.COLUMN_INGREDIENT_BUNDLE;
+import static org.udandroid.bakingapp.data.RecipeContract.RecipeEntry.COLUMN_RECIPE_NAME;
+import static org.udandroid.bakingapp.data.RecipeContract.RecipeEntry.TABLE_NAME;
+import static org.udandroid.bakingapp.data.RecipeContract.RecipeEntry._ID;
+
 /**
  * Created by tommy-thomas on 4/9/18.
  */
@@ -23,10 +29,11 @@ public class RecipeDBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         // Create favorites table (careful to follow SQL formatting rules)
-        final String CREATE_TABLE = "CREATE TABLE "  + RecipeContract.RecipeEntry.TABLE_NAME + " (" +
-                RecipeContract.RecipeEntry._ID + " INTEGER PRIMARY KEY, " +
-                RecipeContract.RecipeEntry.COLUMN_ID + " TEXT NOT NULL, " +
-                RecipeContract.RecipeEntry.COLUMN_INGREDIENT_BUNDLE + " TEXT NOT NULL);";
+        final String CREATE_TABLE = "CREATE TABLE "  + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_ID + " TEXT NOT NULL, " +
+                COLUMN_RECIPE_NAME + " TEXT NOT NULL, " +
+                COLUMN_INGREDIENT_BUNDLE + " TEXT NOT NULL);";
 
         db.execSQL(CREATE_TABLE);
 
