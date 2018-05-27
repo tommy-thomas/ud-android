2f263dd1d68174bcae3eb078324ed5aaa5161768
tommy-thomas <tommyt@uchicago.edu>
2018-05-27 01:34:02 -0500

    Passed idling resource test.

--- BakingApp/app/src/androidTest/java/org/udandroid/bakingapp/ui/IdlingResourceMainActivityTest.java
+++ BakingApp/app/src/androidTest/java/org/udandroid/bakingapp/ui/IdlingResourceMainActivityTest.java
@@ -2,10 +2,17 @@ package org.udandroid.bakingapp.ui;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,14 +21,18 @@ import org.junit.runner.RunWith;
 import org.udandroid.bakingapp.R;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
 public class IdlingResourceMainActivityTest {
 
-    private static int POSITION_TO_TEST = 0;
+    final static int POSITION_TO_TEST = 0;
+    final static String TITLE_TO_TEST = "Nutella Pie";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule =
@@ -39,9 +50,37 @@ public class IdlingResourceMainActivityTest {
     @Test
     public void idlingResourceTest() {
 
-        onView(withId(R.id.rv_recipe))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(POSITION_TO_TEST, click()));
 
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tv_recipe_card), withText(TITLE_TO_TEST),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf. <View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+
+        textView.check(matches(withText(TITLE_TO_TEST)));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher <View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
     @After
